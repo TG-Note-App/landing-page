d1f4adc8cff3df438b80546ca9c910c543557e32
Alex Zhuravlev <leftemail17@mail.ru>
2025-02-26 11:37:49 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './assets/main.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+import { createApp } from 'vue'
+// import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        tgBlack: '#18222D',
+        tgBlue: '#232E3C',
+        tgGrey: 'hsl(215, 13%, 19%)'
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      gridTemplateColumns: {
+        '70/30': '70% 28%',
+      },
+      animation: {
+        'gradient': 'gradient 15s ease infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': {
+            'background-position': '0% 50%',
+          },
+          '50%': {
+            'background-position': '100% 50%',
+          },
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
